33d28ff6c218c1aeb10fa66cb8f689918980134e
Allen Francis <lallenfrancisl@gmail.com>
2021-08-16 23:08:27 +0530

    feat: add login page to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react'
-import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
-import './App.css';
+import {createTheme, StylesProvider, ThemeProvider} from "@material-ui/core";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import "./App.css";
 
 // importing pages
-import Login from './components/login'
+import Login from "./pages/Login/Login";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#6200EE'
+        }
+    }
+});
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        
-        {/* <Route exact path='/'>
-          <Home />
-        </Route> */}
+    return (
+        <StylesProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                          <Redirect to="/login" />
+                        </Route>
 
-        <Route path='/login'>
-          <Login />
-        </Route>
-        
-      </Switch>
-    </Router>
-  );
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
+        </StylesProvider>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Login/Login.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import './Login.css';
+import AuthCard from "../../components/AuthCard/AuthCard";
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="loginPage">
+                <div className="container">
+                    <AuthCard title="Login" btnText="LOGIN" className="loginCard" />
+                    <p className="signupLink">
+                        Don't have an account yet ?
+                        <Link to="/signup">
+                            JOIN NOW !
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
+
+
