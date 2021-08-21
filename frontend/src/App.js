bb70ccc6a2c0ff8dcbc552d0a437d41f2ea69b07
Allen Francis <lallenfrancisl@gmail.com>
2021-08-22 03:22:08 +0530

    fix: merge issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,47 @@
-import React from 'react'
-import {BrowserRouter as Router,Switch,Route,Link,Redirect} from 'react-router-dom'
-
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import { StylesProvider } from '@material-ui/styles';
 import './App.css';
 
 // importing pages
-import Register from './components/register'
-import Login from './components/login'
-import Dashboard from './components/dashboard'
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import { createTheme } from '@material-ui/core';
 
 function App() {
-    return (
-        <StylesProvider injectFirst>
-            <ThemeProvider theme={theme}>
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                          <Redirect to="/login" />
-                        </Route>
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#6200EE',
+      },
+    },
+  });
+  return (
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
 
-                        <Route path="/login">
-                            <Login />
-                        </Route>
-                        <Route path="/signup">
-                            <Signup />
-                        </Route>
-                    </Switch>
-                </Router>
-            </ThemeProvider>
-        </StylesProvider>
-    );
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </StylesProvider>
+  );
 }
 
 export default App;
