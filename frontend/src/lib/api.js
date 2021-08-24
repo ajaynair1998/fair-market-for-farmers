3568f4185576b7f100b74365fd850f450e17e8d7
Allen Francis <lallenfrancisl@gmail.com>
2021-08-25 04:24:15 +0530

    feat: pass cookie along with every request

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const api = axios.create({
   baseURL: 'http://localhost:5000/',
   timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
+    Authorization: Cookies.get('authToken'),
   },
 });
