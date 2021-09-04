070db2a950d74431e394f601c199417929979b06
AJAY <ajaynairarun@gmail.com>
2021-09-04 20:23:57 +0530

    ui done buy product

--- frontend/src/pages/buyProduct/buyProduct.js
+++ frontend/src/pages/buyProduct/buyProduct.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router';
-import { Paper, Tab, Tabs, Typography, Card, Button, CardContent, CardMedia } from '@material-ui/core';
+import
+{
+    Paper, Tab, Tabs, Typography, Card, Button, CardContent, CardMedia,
+    Grid, Slider
+} from '@material-ui/core';
 import { api } from '../../lib/api';
 
 import './buyProduct.css'
@@ -10,7 +14,10 @@ class BuyProduct extends Component
     constructor(props)
     {
         super(props)
-        this.state = { product: "" }
+        this.state = { product: "", selectedStock: 0 }
+
+        // custom
+        this.handleChangeSlider = this.handleChangeSlider.bind(this)
     }
 
     async componentDidMount()
@@ -26,7 +33,7 @@ class BuyProduct extends Component
 
 
             let product = response.data.product
-            console.log(product)
+            // console.log(product)
             this.setState(prevState =>
             {
                 return { ...prevState, product: product }
@@ -39,6 +46,16 @@ class BuyProduct extends Component
         }
 
 
+    }
+
+    handleChangeSlider(e, value)
+    {
+        let changedValue = value
+        // console.log(this.state)
+        this.setState(prevState =>
+        {
+            return { ...prevState, selectedStock: changedValue }
+        })
     }
 
     render()
@@ -57,6 +74,61 @@ class BuyProduct extends Component
                                 image={this.state.product.image}
                                 title={`Product image of ${this.state.product.image.toLowerCase()}`}
                             />
+
+                            <Grid container spacing={3} justifyContent="space-between"
+                                className="price_and_stock">
+                                <Grid item xs={3} sm={2}>
+                                    <Typography variant="p" >
+                                        Stock <b>{this.state.product.stock}</b>
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={3} sm={2}>
+                                    <Typography variant="p1" >
+                                        Price <b>{this.state.product.price}</b>
+                                    </Typography>
+                                </Grid>
+
+                            </Grid>
+
+                            <div className="slider_label">
+                                <Typography variant="p1" >
+                                    Select Amount
+                                </Typography>
+                            </div>
+
+
+                            <Slider
+                                defaultValue={0}
+                                value={this.state.selectedStock}
+                                onDragStop={this.handleChangeSlider}
+                                onChange={this.handleChangeSlider}
+                                aria-labelledby="discrete-slider"
+                                valueLabelDisplay="auto"
+                                step={1}
+                                marks
+                                min={0}
+                                max={this.state.product.stock}
+
+
+                            />
+
+                            <Grid container spacing={3} justifyContent="space-between"
+                                className="cart_and_buy">
+                                <Grid item xs={6} sm={6}>
+                                    <Button variant="outlined" color="secondary" className="button_buy_product">
+                                        Add To Cart
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={6} sm={6}>
+                                    <Button variant="outlined" color="primary" className="button_buy_product">
+                                        Buy Now
+                                    </Button>
+                                </Grid>
+
+                            </Grid>
+
+
+
                         </CardContent>
                     </Card>
                 </div>
